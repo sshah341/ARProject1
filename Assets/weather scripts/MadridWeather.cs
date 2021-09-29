ec4101733ed76bb12b0be4e2c1bd5d1b6b0265c2
Shaili Shah <shailishah@10-5-212-193.vlan876.client.wireless.uic.edu>
2021-09-28 19:53:47 -0500

    AR Project

---
+++ Assets/weather scripts/MadridWeather.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
+
+public class MadridWeather : MonoBehaviour
+{
+    public GameObject weatherTextObject;
+        // add your personal API key after APPID= and before &units=
+       string url = "https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=5e50502d89a72ace392119dabfe658b0";
+
+   
+    void Start()
+    {
+
+    // wait a couple seconds to start and then refresh every 900 seconds
+
+       InvokeRepeating("GetDataFromWeb", 2f, 900f);
+   }
+
+   void GetDataFromWeb()
+   {
+
+       StartCoroutine(GetRequest(url));
+   }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+
+            	// grab the current temperature and simplify it if needed
+            	int startTemp = webRequest.downloadHandler.text.IndexOf("temp",0);
+            	int endTemp = webRequest.downloadHandler.text.IndexOf(",",startTemp);
+				double tempF = float.Parse(webRequest.downloadHandler.text.Substring(startTemp+6, (endTemp-startTemp-6)));
+                int easyTempF = Mathf.RoundToInt((float)tempF);
+                easyTempF = easyTempF - 273;
+                //Debug.Log ("integer temperature is " + easyTempF.ToString());
+                int startConditions = webRequest.downloadHandler.text.IndexOf("main",0);
+                int endConditions = webRequest.downloadHandler.text.IndexOf(",",startConditions);
+                string conditions = webRequest.downloadHandler.text.Substring(startConditions+7, (endConditions-startConditions-8));
+                //Debug.Log(conditions);
+
+                weatherTextObject.GetComponent<TextMeshPro>().text = "" + easyTempF.ToString() + "°C\n" + conditions;
+            }
+        }
+    }
+}
+
---
+++ Assets/weather scripts/classcube8ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class classcube8ball : MonoBehaviour
+{
+    public string place1;
+    bool Reset;
+    bool ResetZ;
+    string[] str1;
+    public AudioClip sound;
+    public AudioSource audio;
+    public GameObject textWeather;
+
+    void Start()
+    {
+
+        Reset = true;
+        if (place1 == "Madrid")
+        {
+            str1 = new string[] { "It is Certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes.", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful" };
+        }
+
+        else
+        {
+            str1 = new string[] { "Go ahead with it", "Stay alert", "Are you sure?", "Maybe you shouldn't", "I would say, YES ", "Seems okay", "NO", "The universe will guide you", "It is the right thing", "STOP right now" };
+        }
+
+
+    }
+
+    
+    void Update()
+    {
+        Debug.Log(transform.rotation.eulerAngles.x);
+
+        if ((transform.rotation.eulerAngles.x > 70 && transform.rotation.eulerAngles.x < 110 && Reset == true))
+        {
+            audio.PlayOneShot(sound);
+            System.Random random = new System.Random();
+            int num = random.Next(0, str1.Length);
+
+            textWeather.GetComponent<TextMeshPro>().text = str1[num];
+            Reset = false;
+        }
+
+        if ((transform.rotation.eulerAngles.z > 70 && transform.rotation.eulerAngles.z < 110 && ResetZ == true))
+        {
+            audio.PlayOneShot(sound);
+            System.Random random = new System.Random();
+            int num = random.Next(0, str1.Length);
+
+            textWeather.GetComponent<TextMeshPro>().text = str1[num];
+            ResetZ = false;
+        }
+        if ((transform.rotation.eulerAngles.x < 30 || transform.rotation.eulerAngles.x > 310))
+        {
+            Reset = true;
+        }
+        if ((transform.rotation.eulerAngles.z < 30 || transform.rotation.eulerAngles.z > 310))
+        {
+            ResetZ = true;
+        }
+    }
+}
